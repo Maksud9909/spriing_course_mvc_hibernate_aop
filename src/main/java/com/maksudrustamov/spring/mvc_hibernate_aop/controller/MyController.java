56cb489bf032ddace9e7f92dc33a3636a09a3e81
maksud9909 <rmaksud822@gmail.com>
2023-03-19 12:45:00 +0500

    Added Service Annotation

--- src/main/java/com/maksudrustamov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/maksudrustamov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -2,6 +2,8 @@ package com.maksudrustamov.spring.mvc_hibernate_aop.controller;
 
 import com.maksudrustamov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.maksudrustamov.spring.mvc_hibernate_aop.entity.Employee;
+import com.maksudrustamov.spring.mvc_hibernate_aop.service.EmployeeService;
+import com.maksudrustamov.spring.mvc_hibernate_aop.service.EmployeeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,11 +14,11 @@ import java.util.List;
 @Controller
 public class MyController {
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
     @RequestMapping("/")
     public String showAllEmployees(Model model){
 
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps",allEmployees); // view будет брать отсюда информацию
 
         return "all-employees";
---
+++ src/main/java/com/maksudrustamov/spring/mvc_hibernate_aop/service/EmployeeServiceImpl.java
@@ -0,0 +1,19 @@
+package com.maksudrustamov.spring.mvc_hibernate_aop.service;
+
+import com.maksudrustamov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
+import com.maksudrustamov.spring.mvc_hibernate_aop.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+    @Autowired
+    private EmployeeDAO employeeDAO;
+    @Transactional
+    public List<Employee> getAllEmployees() {
+        return employeeDAO.getAllEmployees();
+    }
+}
